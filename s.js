6c368f23adc38df14216a0747b2ce58f7bf7e42d
Alexander Sbrodov <51849970+MicroDevApp@users.noreply.github.com>
2026-06-18 11:33:38 +0300

    Update s.js

--- s.js
+++ s.js
@@ -2,80 +2,30 @@
     'use strict';
 
     function HelloComponent(object) {
-        var html = $('<div class="hello-page" style="padding: 3em;"></div>');
-        var last;
-
-        var message = $('<div class="hello-page__message" style="color:#fff; font-size:1.6em; margin-bottom:1.5em; min-height:1.5em;"></div>');
-
-        var btn1 = $('<div class="selector simple-button" style="display:inline-block; margin-right:1em; padding:0.7em 1.5em; background:#28a745; color:#fff; border-radius:0.3em;">Кнопка 1</div>');
-        var btn2 = $('<div class="selector simple-button" style="display:inline-block; padding:0.7em 1.5em; background:#dc3545; color:#fff; border-radius:0.3em;">Кнопка 2</div>');
-
-        var buttons = $('<div class="hello-page__buttons"></div>');
-
-        buttons.append(btn1).append(btn2);
-        html.append(message).append(buttons);
-
-        btn1.on('hover:enter', function () {
-            message.text('Вы нажали Кнопку 1!');
-        });
-
-        btn2.on('hover:enter', function () {
-            message.text('Вы нажали Кнопку 2!');
-        });
-
-        btn1.on('hover:focus', function (e) {
-            last = e.target;
-        });
-
-        btn2.on('hover:focus', function (e) {
-            last = e.target;
-        });
+        this.object = object || {};
 
         this.create = function () {
-            return this.render();
+            this.html = $('<div style="padding: 3em; color: #fff; font-size: 2em; text-align: center;">Привет!</div>');
+
+            this.activity.loader(false);
+            this.activity.toggle();
         };
 
         this.render = function () {
-            return html;
+            return this.html;
         };
 
-        this.start = function () {
-            Lampa.Controller.add('content', {
-                toggle: function () {
-                    Lampa.Controller.collectionSet(html);
-                    Lampa.Controller.collectionFocus(last || false, html);
-                },
-                left: function () {
-                    if (Navigator.canmove('left')) Navigator.move('left');
-                    else Lampa.Controller.toggle('menu');
-                },
-                right: function () {
-                    Navigator.move('right');
-                },
-                up: function () {
-                    if (Navigator.canmove('up')) Navigator.move('up');
-                    else Lampa.Controller.toggle('head');
-                },
-                down: function () {
-                    if (Navigator.canmove('down')) Navigator.move('down');
-                },
-                back: function () {
-                    Lampa.Activity.backward();
-                }
-            });
-
-            Lampa.Controller.toggle('content');
-        };
-
+        this.start = function () {};
         this.pause = function () {};
         this.stop = function () {};
 
-        this.destroy = function () {
-            html.remove();
+        this.back = function () {
+            Lampa.Activity.backward();
         };
 
-        this.activity.loader(false);
-        this.activity.toggle();
+        this.destroy = function () {
+            this.html.remove();
+        };
     }
 
     function startPlugin() {
