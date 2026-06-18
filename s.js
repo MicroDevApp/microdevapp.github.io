465dda9f2d640812613e9fce44149ae13f6c9696
Alexander Sbrodov <51849970+MicroDevApp@users.noreply.github.com>
2026-06-18 09:56:43 +0300

    Update s.js

--- s.js
+++ s.js
@@ -6,11 +6,15 @@
 
     function startPlugin() {
         Lampa.Menu.addButton(
-            '<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2C6 2 3 5 3 10c0 4 3 7 7 7s7-3 7-7c0-5-3-8-7-8z" fill="#E74C3C"/><path d="M10 5c-2 0-4 1-4 3 0 2 2 3 4 3s4-1 4-3c0-2-2-3-4-3z" fill="#C0392B"/></svg>',
+            '<svg height="20" width="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="currentColor"/></svg>',
             'Привет',
             function () {
+                // Добавляем случайный параметр, чтобы каждый раз грузить
+                // свежую версию main.html, а не кешированную браузером
+                var freshUrl = HTML_URL + '?nocache=' + Date.now();
+
                 Lampa.Iframe.show({
-                    url: HTML_URL,
+                    url: freshUrl,
                     onBack: function () {
                         // вызывается, когда пользователь нажал "назад"
                     }
