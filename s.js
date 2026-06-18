fc5d8ddb6fac0f6116f69acea74fb601fe748407
Alexander Sbrodov <51849970+MicroDevApp@users.noreply.github.com>
2026-06-18 08:04:47 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,4 +1,37 @@
 (function() {
-    alert('ПЛАГИН ЗАГРУЖЕН!');
-    console.log('--- ПЛАГИН ЗАГРУЖЕН ---');
+    'use strict';
+
+    function init() {
+        // Убедимся, что мы добавляем объект с обязательными полями
+        Lampa.Settings.push({
+            name: 'my_test_plugin', // Уникальный идентификатор
+            label: 'Моя страница',   // Заголовок, который видит пользователь
+            type: 'button',
+            onClick: function() {
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'Приветствие', // Заголовок страницы
+                    component: 'test_page',
+                    page: 1
+                });
+            }
+        });
+
+        Lampa.Component.add('test_page', function() {
+            this.create = function() {
+                var div = document.createElement('div');
+                div.style.padding = '50px';
+                div.style.textAlign = 'center';
+                div.innerHTML = '<h1>Привет!</h1>';
+                return div;
+            };
+            this.render = function() { return this.create(); };
+        });
+    }
+
+    if (window.lampa) {
+        init();
+    } else {
+        window.addEventListener('lampa:ready', init);
+    }
 })();
