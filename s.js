cee18c5768563ac5d56914d3de0d69e5c4e79fa2
sanyazavod <51849970+sanyazavod@users.noreply.github.com>
2026-06-18 21:37:50 +0300

    565

--- s.js
+++ s.js
@@ -1,32 +1,73 @@
 (function () {
     'use strict';
 
-    function MainPage() {
+    var HTML_URL = 'https://microdevapp.github.io/main.html';
+
+    function HelloComponent(object) {
+        var html = $('<div style="width:100%; height:100%;"></div>');
+        var frame = $('<iframe style="width:100%; height:100%; border:none;"></iframe>');
+
+        html.append(frame);
+
+        this.create = function () {
+            return this.render();
+        };
 
         this.render = function () {
+            return html;
+        };
 
-            return $(
-                '<div style="width:100%;height:100%;">' +
-                    '<iframe src="https://microdevapp.github.io/main.html?nocache=' + Date.now() + '"' +
-                    ' style="width:100%;height:100%;border:none;"></iframe>' +
-                '</div>'
-            );
+        this.start = function () {
+            var self = this;
+
+            // Добавляем nocache при каждом открытии
+            frame.attr('src', HTML_URL + '?nocache=' + Date.now());
+
+            Lampa.Controller.add('content', {
+                toggle: function () {},
+                left: function () { Lampa.Controller.toggle('menu'); },
+                right: function () {},
+                up: function () { Lampa.Controller.toggle('head'); },
+                down: function () {},
+                back: function () { Lampa.Activity.backward(); }
+            });
+
+            Lampa.Controller.toggle('content');
+
+            self.activity.loader(false);
+            self.activity.toggle();
+        };
+
+        this.pause = function () {};
+        this.stop = function () {};
+
+        this.destroy = function () {
+            html.remove();
         };
     }
 
-    Lampa.Component.add('main_page', MainPage);
+    function startPlugin() {
+        Lampa.Component.add('hello_page', HelloComponent);
 
-    Lampa.Menu.addButton(
-        '<svg height="20" width="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="currentColor"/></svg>',
-        'Привет',
-        function () {
+        Lampa.Menu.addButton(
+            '<svg height="20" width="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="currentColor"/></svg>',
+            'Привет',
+            function () {
+                Lampa.Activity.push({
+                    title: 'Привет',
+                    component: 'hello_page'
+                });
+            }
+        );
+    }
 
-            Lampa.Activity.push({
-                title: 'Привет',
-                component: 'main_page'
-            });
-
-        }
-    );
-
+    if (window.appready) {
+        startPlugin();
+    } else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') {
+                startPlugin();
+            }
+        });
+    }
 })();
