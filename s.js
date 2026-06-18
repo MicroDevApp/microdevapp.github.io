c3998432c77e01b823db462cb28fccac7c6a30e9
Alexander Sbrodov <51849970+MicroDevApp@users.noreply.github.com>
2026-06-18 09:29:05 +0300

    1

--- s.js
+++ s.js
@@ -1,30 +1,89 @@
 (function () {
     'use strict';
 
+    // Укажите здесь полный URL, по которому реально доступен main.html
+    // (тот же хостинг, где лежит и этот s.js)
+    var HTML_URL = 'https://microdevapp.github.io/main.html';
+
     function HelloComponent(object) {
-        this.object = object || {};
+        var html = $('<div></div>');
+        var last;
 
         this.create = function () {
-            this.html = $('<div style="padding: 3em; color: #fff; font-size: 2em; text-align: center;">Привет!</div>');
-
-            this.activity.loader(false);
-            this.activity.toggle();
+            return this.render();
         };
 
         this.render = function () {
-            return this.html;
+            return html;
         };
 
-        this.start = function () {};
+        this.start = function () {
+            var self = this;
+
+            $.get(HTML_URL, function (markup) {
+                html.html(markup);
+
+                var message = html.find('.hello-page__message');
+                var btn1 = html.find('.hello-page__btn-1');
+                var btn2 = html.find('.hello-page__btn-2');
+
+                btn1.on('hover:enter', function () {
+                    message.text('Вы нажали Кнопку 1!');
+                });
+
+                btn2.on('hover:enter', function () {
+                    message.text('Вы нажали Кнопку 2!');
+                });
+
+                btn1.on('hover:focus', function (e) {
+                    last = e.target;
+                });
+
+                btn2.on('hover:focus', function (e) {
+                    last = e.target;
+                });
+
+                Lampa.Controller.add('content', {
+                    toggle: function () {
+                        Lampa.Controller.collectionSet(html);
+                        Lampa.Controller.collectionFocus(last || false, html);
+                    },
+                    left: function () {
+                        if (Navigator.canmove('left')) Navigator.move('left');
+                        else Lampa.Controller.toggle('menu');
+                    },
+                    right: function () {
+                        Navigator.move('right');
+                    },
+                    up: function () {
+                        if (Navigator.canmove('up')) Navigator.move('up');
+                        else Lampa.Controller.toggle('head');
+                    },
+                    down: function () {
+                        if (Navigator.canmove('down')) Navigator.move('down');
+                    },
+                    back: function () {
+                        Lampa.Activity.backward();
+                    }
+                });
+
+                self.activity.loader(false);
+                self.activity.toggle();
+
+                Lampa.Controller.toggle('content');
+            }).fail(function () {
+                html.html('<div style="padding:3em; color:#fff;">Не удалось загрузить страницу</div>');
+
+                self.activity.loader(false);
+                self.activity.toggle();
+            });
+        };
+
         this.pause = function () {};
         this.stop = function () {};
 
-        this.back = function () {
-            Lampa.Activity.backward();
-        };
-
         this.destroy = function () {
-            this.html.remove();
+            html.remove();
         };
     }
 
@@ -32,11 +91,11 @@
         Lampa.Component.add('hello_page', HelloComponent);
 
         Lampa.Menu.addButton(
-            '<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2C6 2 3 5 3 10c0 4 3 7 7 7s7-3 7-7c0-5-3-8-7-8z" fill="#E74C3C"/><path d="M10 5c-2 0-4 1-4 3 0 2 2 3 4 3s4-1 4-3c0-2-2-3-4-3z" fill="#C0392B"/></svg>',
-            'Твій розділ',
+            '<svg height="20" width="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="currentColor"/></svg>',
+            'Привет',
             function () {
                 Lampa.Activity.push({
-                    title: 'Олександр',
+                    title: 'Привет',
                     component: 'hello_page'
                 });
             }
