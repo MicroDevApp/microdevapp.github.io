b4ab8f2224f2edfbdcf21ede500ff30fe6eedc77
sanyazavod <51849970+sanyazavod@users.noreply.github.com>
2026-06-18 21:11:05 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,24 +1,49 @@
 (function () {
     'use strict';
 
-    // Полный URL вашей страницы main.html (должен реально быть доступен по сети)
     var HTML_URL = 'https://microdevapp.github.io/main.html';
 
+    function MainPage() {
+
+        this.create = function () {
+
+            this.activity.loader(false);
+
+            this.render = $(
+                '<div style="width:100%;height:100%;">' +
+                    '<iframe ' +
+                        'src="' + HTML_URL + '?nocache=' + Date.now() + '" ' +
+                        'style="width:100%;height:100%;border:none;">' +
+                    '</iframe>' +
+                '</div>'
+            );
+        };
+
+        this.start = function () {
+            this.activity.toggle();
+            this.activity.append(this.render);
+        };
+
+        this.destroy = function () {
+            if (this.render) this.render.remove();
+        };
+    }
+
     function startPlugin() {
+
+        Lampa.Component.add('main_page', MainPage);
+
         Lampa.Menu.addButton(
             '<svg height="20" width="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="currentColor"/></svg>',
             'Привет',
             function () {
-                // Добавляем случайный параметр, чтобы каждый раз грузить
-                // свежую версию main.html, а не кешированную браузером
-                var freshUrl = HTML_URL + '?nocache=' + Date.now();
 
-                Lampa.Iframe.show({
-                    url: freshUrl,
-                    onBack: function () {
-                        // вызывается, когда пользователь нажал "назад"
-                    }
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'Привет',
+                    component: 'main_page'
                 });
+
             }
         );
     }
@@ -27,9 +52,10 @@
         startPlugin();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
+            if (e.type === 'ready') {
                 startPlugin();
             }
         });
     }
+
 })();
