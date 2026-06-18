24a58cc5ebee9dec925e97c361d46ebf04b398be
Alexander Sbrodov <51849970+MicroDevApp@users.noreply.github.com>
2026-06-18 11:18:58 +0300

    Update s.js

--- s.js
+++ s.js
@@ -1,36 +1,10 @@
 (function () {
     'use strict';
 
-    // ссылка на отдельный файл — например, через jsDelivr для GitHub-репозитория:
-    // https://cdn.jsdelivr.net/gh/USER/REPO@main/hello.html
-    var pageUrl = 'https://cdn.jsdelivr.net/gh/USER/REPO@main/hello.html';
-
     function HelloComponent(object) {
-        var html = $('<div class="hello-page" style="position:absolute; top:0; left:0; right:0; bottom:0;"></div>');
-        var iframe = $('<iframe class="hello-page__frame" frameborder="0" style="position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%; border:0; background:#15151a;"></iframe>');
+        var html = $('<div class="hello-page" style="padding:3em;"></div>');
+        var iframe = $('<iframe style="width:100%; height:70vh; border:0; display:block; background:lime;" srcdoc="<body style=\'background:lime;color:#000;font-size:3em;\'>ЭТО IFRAME</body>"></iframe>');
         html.append(iframe);
-
-        function onMessage(e) {
-            if (!e.data || typeof e.data !== 'object') return;
-            if (e.data.type === 'back') {
-                Lampa.Activity.backward();
-            }
-        }
-
-        function loadPage() {
-            fetch(pageUrl)
-                .then(function (resp) {
-                    if (!resp.ok) throw new Error('HTTP ' + resp.status);
-                    return resp.text();
-                })
-                .then(function (text) {
-                    iframe.attr('srcdoc', text);
-                })
-                .catch(function (err) {
-                    console.error('hello_page: не удалось загрузить страницу', err);
-                    iframe.attr('srcdoc', '<body style="background:#15151a;color:#fff;font-family:Arial;padding:2em;">Ошибка загрузки страницы</body>');
-                });
-        }
 
         this.create = function () {
             return this.render();
@@ -41,38 +15,20 @@
         };
 
         this.start = function () {
-            window.addEventListener('message', onMessage);
-
             Lampa.Controller.add('content', {
                 toggle: function () {
                     Lampa.Controller.collectionSet(html);
-                    var node = iframe.get(0);
-                    if (node) node.focus();
                 },
-                left: function () {},
-                right: function () {},
-                up: function () {},
-                down: function () {},
                 back: function () {
                     Lampa.Activity.backward();
                 }
             });
-
             Lampa.Controller.toggle('content');
-
-            loadPage();
         };
 
         this.pause = function () {};
-
-        this.stop = function () {
-            window.removeEventListener('message', onMessage);
-        };
-
-        this.destroy = function () {
-            window.removeEventListener('message', onMessage);
-            html.remove();
-        };
+        this.stop = function () {};
+        this.destroy = function () { html.remove(); };
 
         this.activity.loader(false);
         this.activity.toggle();
@@ -84,10 +40,7 @@
             '<svg height="20" width="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="currentColor"/></svg>',
             'Привет',
             function () {
-                Lampa.Activity.push({
-                    title: 'Привет',
-                    component: 'hello_page'
-                });
+                Lampa.Activity.push({ title: 'Привет', component: 'hello_page' });
             }
         );
     }
@@ -96,9 +49,7 @@
         startPlugin();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
-                startPlugin();
-            }
+            if (e.type == 'ready') startPlugin();
         });
     }
 })();
